4fe2a580f6e037bb6334d9ef1cd4b7d0022d9016
Mina Wanis <mwanis@turing.une.edu.au>
2023-07-23 00:47:54 +1000

    The rest of the work add API server and Cinema Class

--- une-cinema/app.ts
+++ une-cinema/app.ts
@@ -15,14 +15,17 @@ class Seat {
         this.element = document.createElement('div')
         this.element.classList.add('seat')
         this.element.classList.add(this.status.toLowerCase())
-        this.element.addEventListener('click', this.handleClick.bind(this))
-
+        this.element.addEventListener('click', () => {
+            this.handleClick()
+        }
+        )
     }
 
     handleClick () {
         if (this.status === STATUS.OCCUPIED) return 
         this.element.classList.remove(this.status.toLowerCase())
-        this.status = this.status === STATUS.AVAILABLE ? STATUS.SELECTED : STATUS.AVAILABLE
+        this.status = 
+            this.status === STATUS.AVAILABLE ? STATUS.SELECTED : STATUS.AVAILABLE
         this.element.classList.add(this.status.toLowerCase())
 
     }
@@ -39,11 +42,11 @@ class Row {
     seats: Seat[]
     element: HTMLDivElement
 
-    constructor(id:number, seatNumber: number) {
+    constructor(id:number, seatNumber: number, occupiedSeats: number[] = []) {
         this.id = id
         this.seats = Array.from({length: seatNumber }).map((_, index) => {
-            const seatID = id * seatNumber + index + 1
-            return new Seat(seatID)
+            const seatID = (id * seatNumber) + index
+            return new Seat(seatID, occupiedSeats.includes(seatID))
         })
         this.element = document.createElement('div')
         this.element.classList.add('row')
@@ -62,17 +65,21 @@ class SeatMap {
     selectedSeats: number[] = []
     element: HTMLDivElement
 
-    constructor( rowNumber: number, seatNumberPerRow: number) {
-        this.rows = Array.from({length: rowNumber}).map((_, index) => {
-            return new Row(index, seatNumberPerRow)
-    })
-    this.element = document.createElement('div')
-    this.element.classList.add('seat-map')
-    this.element.append(...this.rows.map(row => row.element))
-    this.element.addEventListener('click', () => {
-        this.getSelectedSeatsID()
-    })
-    }
+    constructor( 
+        rowNumber: number,
+        seatNumberPerRow: number,
+        occupiedSeats: number[] = []
+        ) {
+            this.rows = Array.from({length: rowNumber}).map((_, index) => {
+                return new Row(index, seatNumberPerRow, occupiedSeats)
+        })
+        this.element = document.createElement('div')
+        this.element.classList.add('seat-map')
+        this.element.append(...this.rows.map(row => row.element))
+        this.element.addEventListener('click', () => {
+            this.getSelectedSeatsID()
+        })
+        }
 
     getSelectedSeatsID() {
         // this.selectedSeats = this.rows.reduce<number[]>((total, row) => {
@@ -85,6 +92,184 @@ class SeatMap {
     
 }
 
+
+type Theatre = {
+    rowNumber: number,
+    seatNumberPerRow: number,
+    occupiedSeats: number[]
+}
+
+type Movie = {
+    id: number,
+    title: string,
+    poster: string,
+    theatre: Theatre
+}
+
+
+async function http<T>(request: RequestInfo): Promise<T> {
+    const response = await fetch(request)
+    if (!response.ok) {
+        throw new Error(response.statusText)
+    }
+    const headers = response.headers
+    const data = headers.get('Content-Type')?.includes('application/json')
+        ? await response.json()
+        : {}
+    return data
+}
+
+ 
+// async function fetchMovies(): Promise<void>{
+//     const movies = await http<Movie[]>('http://localhost:5000/movies')
+//     console.log(movies)
+
+// }
+
+
+
+// class Cinema {
+//     movies: Movie[] = []
+//     seatMap: SeatMap | null = null
+//     theatreContainer: HTMLDivElement
+//     moviesContainer: HTMLDivElement
+//     movieElements: HTMLDivElement [] = []
+
+//     constructor(){
+//         this.moviesContainer = document.createElement('div')
+//         this.moviesContainer.id = 'movies'
+//         this.moviesContainer.classList.add('movies')
+//         this.theatreContainer = document.createElement('div')
+//         this.theatreContainer.id = 'theatre'
+//         this.theatreContainer.classList.add('theatre')
+//         const screenElement = document.createElement('div')
+//         screenElement.classList.add('screen')
+//         screenElement.innerText = 'Screen'
+//         this.theatreContainer.appendChild(screenElement)
+//         document
+//             .getElementById('cinema')
+//             ?.append(this.moviesContainer, this.theatreContainer)
+
+//     }
+
+//     async fetchMovies() {
+//         this.movies = await http<Movie[]>('http://localhost:5000/movies')
+//         if(this.movies.length) {
+//             this.renderMovies()
+//             this.selectMovie(2)
+//         }
+//     }
+
+//     renderMovies() {
+//         this.movieElements = []
+//         this.moviesContainer.innerHTML = ''
+//         this.movies.forEach((movie, index) => {
+//             const movieElement = document.createElement('div')
+//             movieElement.id = movie.id.toString()
+//             movieElement.classList.add('movie')
+//             movieElement.addEventListener('click', () => {
+//                 if (!movieElement.classList.contains('selected')) {
+//                     this.selectMovie(index)
+//                 }
+//             })
+//             const posterElement = document.createElement('img')
+//             posterElement.classList.add('poster')
+//             posterElement.src = movie.poster
+//             const titleElement = document.createElement('div')
+//             titleElement.classList.add('title')
+//             titleElement.innerText = movie.title
+//             this.movieElements.push(movieElement)
+//             this.moviesContainer.appendChild(movieElement)
+
+//         })
+//     }
+
+    type MovieElement = {
+        element: HTMLDivElement,
+        movie: Movie
+    }
+
+    class Cinema {
+        movies: Movie[] = []
+        seatMap: SeatMap | null = null
+        theatreContainer: HTMLDivElement
+        moviesContainer: HTMLDivElement
+        movieElements: MovieElement[] = []
+
+        constructor(){
+            this.moviesContainer = document.createElement('div')
+            this.moviesContainer.id = 'movies'
+            this.moviesContainer.classList.add('movies')
+            this.theatreContainer = document.createElement('div')
+            this.theatreContainer.id = 'theatre'
+            this.theatreContainer.classList.add('theatre')
+            const screenElement = document.createElement('div')
+            screenElement.classList.add('screen')
+            screenElement.innerText = 'Screen'
+            this.theatreContainer.appendChild(screenElement)
+            document
+                .getElementById('cinema')
+                ?.append(this.moviesContainer, this.theatreContainer)
+
+        }
+
+        async fetchMovies() {
+            this.movies = await http<Movie[]>('http://localhost:5000/movies')
+            if(this.movies.length) {
+                this.renderMovies()
+                this.selectMovie(this.movies[0])
+            }
+        }
+
+        renderMovies() {
+            this.movieElements = []
+            this.moviesContainer.innerHTML = ''
+            this.movies.forEach((movie, index) => {
+                const movieElement = document.createElement('div')
+                movieElement.id = movie.id.toString()
+                movieElement.classList.add('movie')
+                movieElement.addEventListener('click', () => {
+                    if (!movieElement.classList.contains('selected')) {
+                        this.selectMovie(movie)
+                    }
+                })
+                const posterElement = document.createElement('img')
+                posterElement.classList.add('poster')
+                posterElement.src = movie.poster
+                const titleElement = document.createElement('div')
+                titleElement.classList.add('title')
+                titleElement.innerText = movie.title
+                movieElement.appendChild(posterElement)
+                movieElement.appendChild(titleElement)
+                this.movieElements.push({element: movieElement, movie})
+                this.moviesContainer.appendChild(movieElement)
+
+            })
+        }
+
+        selectMovie(selectedMovie: Movie) {
+            this.movieElements.forEach(({element, movie}) => 
+                selectedMovie.id === movie.id
+                ? element.classList.add('selected')
+                : element.classList.remove('selected')
+            )
+            if (selectedMovie.theatre) {
+                const {
+                    theatre: {rowNumber, seatNumberPerRow, occupiedSeats}, 
+                } = selectedMovie
+                if (this.seatMap) {
+                    this.theatreContainer.lastChild?.remove()
+                }
+                this.seatMap = new SeatMap(rowNumber, seatNumberPerRow, occupiedSeats)
+                this.theatreContainer.appendChild(this.seatMap.element)
+            }
+        }  
+    }
+// }
+
+
+const cinema = new Cinema()
+cinema.fetchMovies()
 
 
 const seatMap = new SeatMap (8,8)
---
+++ une-cinema/server.ts
@@ -0,0 +1,54 @@
+import express, { Express, Request, Response } from 'express'
+import cors from 'cors' 
+
+
+const app: Express = express()
+
+app.use(cors());
+
+const movies = [
+    {
+        id: 1,
+        title: 'Jurassic World Dominion',
+        poster:
+            'https://cdn.eventcinemas.com.au/cdn/resources/movies/15020/images/largeposter.jpg',
+        theater: {
+            rowNumber: 8,
+            seatNumberPerRow: 8,
+            occupiedSeats: [2, 6, 12, 3],
+
+        },
+    },
+    {
+        id: 2,
+        title: 'Top Gun: Maverick',
+        poster:
+            'https://cdn.eventcinemas.com.au/cdn/resources/movies/14621/images/largeposter.jpg',
+        theater: {
+            rowNumber: 10,
+            seatNumberPerRow: 12,
+            occupiedSeats: [3, 4, 13, 14],
+        },
+    },
+    {
+        id: 3,
+        title: 'Doctor Strange in the Multiverse of Madness',
+        poster:
+            'https://cdn.eventcinemas.com.au/cdn/resources/movies/15657/images/largeposter.jpg',
+        theater: {
+            rowNumber: 9,
+            seatNumberPerRow: 13,
+            occupiedSeats: [15, 16, 22, 3, 5],
+        },
+    },
+];
+
+app.get('/movies', (req: Request, res: Response) => {
+    res.send(movies);
+});
+
+app.listen(5000, () => {
+    console.log('[server]: Server is running at http://localhost:5000/movies');
+})
+
+
